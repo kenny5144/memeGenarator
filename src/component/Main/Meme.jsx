24b7c65d631d66854ed810c5d3f40bd6fbf9a97a
kehinde ojo <ojok761@gmail.com>
2024-02-16 23:21:54 -0500

    added

--- src/component/Main/Meme.jsx
+++ src/component/Main/Meme.jsx
@@ -1,26 +1,40 @@
-import "./Meme.css"
+import "./Meme.css";
 const Meme = () => {
-  const handleInp =()=>{
-    console.log("hello my name is kenny ")
-  }
+  const handleInp = () => {
+    console.log("hello my name is kenny ");
+  };
   return (
     <>
       <div>
-        <span   className="info">
-          <label htmlFor="">Top text</label>
+        <span className="info">
+          <span >
+            <label htmlFor="">Top text</label>
+            <br/>
 
-        <input onChange={handleInp} type="text"  id="" placeholder='Top Text'/>
-        <label htmlFor="">Bottom text</label>
-        
-        <input onChange={handleInp} type="text"  id="" placeholder='Bottom text' />
+            <input
+              onChange={handleInp}
+              type="text"
+              id=""
+              placeholder="Top Text"
+            />
+          </span>
+          <span className="input">
+            <label htmlFor="">Bottom text</label>
+            <br />
+            <input
+              onChange={handleInp}
+              type="text"
+              id=""
+              placeholder="Bottom text"
+            />
+          </span>
         </span>
         <br />
 
-        <button className='mainBtn'> Get a new meme image 🖼 </button>
-
+        <button className="mainBtn"> Get a new meme image 🖼 </button>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Meme
+export default Meme;
